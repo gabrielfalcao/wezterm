4b483c17236bf9551b390e8005c1cc358195b6da
Gabriel Falcão <gabrielteratos@gmail.com>
2025-11-21 15:18:42 +0000

    saves fix-weird-filenames.py

--- gabrielfalcao/workbench/2025-11-21/fix-weird-filenames.py
+++ gabrielfalcao/workbench/2025-11-21/fix-weird-filenames.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 import re
 import os
+import sys
 import shutil
+import unicodedata
 from typing import Tuple
 from os.path import splitext
 from pathlib import Path
 
 cwd = Path(os.getcwd()).absolute()
 regex = re.compile(r'[^a-z0-9_.@^%(~) -]+', re.I)
+
+def normalized_string(s: str) -> str:
+    return unicodedata.normalize('NFKC', str(s))
 
 def split(path: Path) -> Path:
     parent = path.parent
@@ -32,11 +37,26 @@ def fix_weird(tup: Tuple[Path, str, str]) -> Path:
     return parent.joinpath(old_name), parent.joinpath(new_name)
 
 
-def main():
-    files = map(split, cwd.iterdir())
+def fix_on_directory(working_dir: Path):
+    if not working_dir.isdir():
+        raise RuntimeError(f'cannot "fix_on_directory" on a path that is not a directory: {path}')
+    files = map(split, working_dir.iterdir())
     weird = filter(is_weird, files)
     for old, new in map(fix_weird, weird):
+        n_old = normalized_string(old)
+        n_new = normalized_string(new)
+        print(f"renaming {n_old} to {n_new}")
         shutil.move(old, new)
+
+def main():
+    directory_args = list(filter(lambda path: path.isdir(), map(lambda arg: Path(arg).expanduser().absolute(), sys.argv)))
+    total_dirs = len(directory_args)
+    if total_dirs is 0:
+        print(f"no target directories provided via command-line, defaulting to the current: {cwd}", file=sys.stderr)
+        fix_on_directory(cwd)
+        return
+    else:
+        list(map(fix_on_directory, directory_args))
 
 if __name__ == '__main__':
     main()
