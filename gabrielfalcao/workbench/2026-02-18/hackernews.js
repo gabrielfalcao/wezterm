a59a876689eada21b439f199c8622cb9b9418fd2
Gabriel Falcão <gabrielteratos@gmail.com>
2026-02-18 19:54:41 +0000

    saves hackernews.js

--- gabrielfalcao/workbench/2026-02-18/hackernews.js
+++ gabrielfalcao/workbench/2026-02-18/hackernews.js
@@ -1,23 +1,19 @@
-(() =>
-  [console.log, copy].reduce(
-    (text, fn) => {
-      const urlsAndTitles = Array.from(
-        new Set(document.querySelectorAll(`table td span.titleline a[href]`)),
-      ).map((el) => {
-        let url = null;
-        try {
-          url = URL(`${el.href}`);
-        } catch (_) {
-          url = URL(`https://news.ycombinator.com${el.href}`);
-        }
-        const title = el.innerText;
-        return { title, url };
-      });
-      const urls = urlsAndTitles.map(({ url }) => url.toString());
-      const urlCommentTitleList = urlsAndTitles
-        .map(({ title, url }) => `"${url}"    // ${title}`)
-        .join("\n");
-      return urlCommentTitleList
-    },
-    [fn(text), text].pop(),
-  ))();
+(() => {
+  const urlsAndTitles = Array.from(
+    new Set(document.querySelectorAll(`table td span.titleline a[href]`)),
+  ).map((el) => {
+    let url = null;
+    try {
+      url = URL(`${el.href}`);
+    } catch (_) {
+      url = URL(`https://news.ycombinator.com${el.href}`);
+    }
+    const title = el.innerText;
+    return { title, url };
+  });
+  const urls = urlsAndTitles.map(({ url }) => url.toString());
+  const urlCommentTitleList = urlsAndTitles
+    .map(({ title, url }) => `"${url}"    // ${title}`)
+    .join("\n");
+  return urlCommentTitleList;
+})();
