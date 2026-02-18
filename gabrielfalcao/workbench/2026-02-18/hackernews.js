c568a5ca32e2b4abbc59b4222513effe0377368b
Gabriel Falcão <gabrielteratos@gmail.com>
2026-02-18 20:01:14 +0000

    saves hackernews.js

--- gabrielfalcao/workbench/2026-02-18/hackernews.js
+++ gabrielfalcao/workbench/2026-02-18/hackernews.js
@@ -1,4 +1,8 @@
 (() => {
+  const printerContext {
+    widestTitleLength: 0,
+    widestUrlLength: 0,
+  };
   const urlsAndTitles = Array.from(
     new Set(document.querySelectorAll(`table td span.titleline a[href]`)),
   ).map((el) => {
@@ -9,12 +13,20 @@
       url = new URL(`https://news.ycombinator.com${el.href}`);
     }
     const title = el.innerText;
+    if (title.length > printerContext.widestTitleLength) {
+      widestTitleLength=title.length;
+    }
+    if (url.length > printerContext.widestUrlLength) {
+      widestUrlLength=url.length;
+    }
     return { title, url };
   });
   const urls = urlsAndTitles.map(({ url }) => url.toString());
   const urlCommentTitleList = urlsAndTitles
     .map(({ title, url }) => `"${url}"    // ${title}`)
     .join("\n");
-  console.log({ urlsAndTitles, urls, urlCommentTitleList });
-  return urlCommentTitleList;
-})();
+  /* console.log({ urlsAndTitles, urls, urlCommentTitleList });
+   * console.log(urlCommentTitleList); */
+  const urlContext = { urlsAndTitles, urls, urlCommentTitleList };
+    return {...printerContext, ...urlContext} }
+})() ? null: null;
